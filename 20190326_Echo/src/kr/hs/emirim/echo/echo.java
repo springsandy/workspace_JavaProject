06e902d3c0db2542b70a54d9e5ae7c6a27919943
springsandy <s2018w26@e-mirim.hs.kr>
2019-03-26 19:46:58 +0900

    add Echo project

--- 20190326_Echo/src/kr/hs/emirim/echo/echo.java
+++ 20190326_Echo/src/kr/hs/emirim/echo/echo.java
@@ -1,10 +1,20 @@
 package kr.hs.emirim.echo;
 
+import java.util.Scanner;
+
 public class echo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner=new Scanner(System.in);
+		while(true) {
+			System.out.print("입력하시오.");
+			String an=scanner.nextLine();
+			if(an.equals("exit")) {
+				System.out.println("끝");
+				break;
+			}
+			System.out.println(an);
+		}
 	}
 
 }
