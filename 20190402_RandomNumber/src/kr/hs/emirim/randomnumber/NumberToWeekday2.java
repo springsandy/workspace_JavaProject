ef6e06ba45cb6479ed99e05773a4c4c39f21c566
springsandy <s2018w26@e-mirim.hs.kr>
2019-04-02 19:51:49 +0900

    NumberToWeekday

--- 20190402_RandomNumber/src/kr/hs/emirim/randomnumber/NumberToWeekday2.java
+++ 20190402_RandomNumber/src/kr/hs/emirim/randomnumber/NumberToWeekday2.java
@@ -1,5 +1,13 @@
 package kr.hs.emirim.randomnumber;
 
-public class NumberToWeekday2 {
+import java.util.Scanner;
 
+public class NumberToWeekday2 {
+	public static void main(String[] args) {
+		String[] weekdays= {"월","화","수","목","금","토","일"};
+		
+		Scanner scanner=new Scanner(System.in);
+		int inputNumber=scanner.nextInt();
+		System.out.println(weekdays[inputNumber-1]+"요일");
+	}
 }
