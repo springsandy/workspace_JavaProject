94d872fc1c639030f561d0cbc052ee256e97e7da
springsandy <s2018w26@e-mirim.hs.kr>
2019-05-21 18:57:25 +0900

    EXOBTS

---
+++ 20190430_GUI/src/GUITest4_JRedioButton.java
@@ -0,0 +1,30 @@
+import java.awt.Dimension;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+
+public class GUITest4_JRedioButton {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		frame.setPreferredSize(new Dimension(400, 600));
+		
+		JRadioButton exoButton = new JRadioButton("EXO");
+		JRadioButton btsButton = new JRadioButton("BTS");
+		ButtonGroup bg = new ButtonGroup();
+		bg.add(exoButton);
+		bg.add(btsButton);
+		
+		panel.add(exoButton);
+		panel.add(btsButton);
+		frame.add(panel);
+		
+		frame.pack();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
