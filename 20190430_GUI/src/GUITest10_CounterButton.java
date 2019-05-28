1b50418ad71220af980a701dad4c1bfc8d3b93cc
springsandy <s2018w26@e-mirim.hs.kr>
2019-05-28 19:52:04 +0900

    Action

--- 20190430_GUI/src/GUITest10_CounterButton.java
+++ 20190430_GUI/src/GUITest10_CounterButton.java
@@ -1,11 +1,15 @@
 
 
 import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class GUITest10_CounterButton {
 
@@ -15,8 +19,22 @@ public class GUITest10_CounterButton {
 		frame.setPreferredSize(new Dimension(300,100));
 
 		JLabel lbNumber = new JLabel("0");
+		lbNumber.setHorizontalAlignment(SwingConstants.CENTER);
 		JButton  btCount = new JButton("Click Counter");
 		
+		ActionListener listener = new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String number = lbNumber.getText();
+				int n = Integer.parseInt(number);
+				String result = Integer.toString(n+1);
+				lbNumber.setText(result);
+			}
+		};
+		btCount.addActionListener(listener);
+		
+		GridLayout gl = new GridLayout(1,2);		
+		panel.setLayout(gl);
 		panel.add(lbNumber);
 		panel.add(btCount);
 		
